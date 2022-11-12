d59cfcad2e39f2fbfb91be1ac0f791de142b4fa2
David Marx <david.marx84@gmail.com>
2022-11-12 14:20:04 -0800

    added simple helper

--- src/keyframed/__init__.py
+++ src/keyframed/__init__.py
@@ -28,6 +28,20 @@ def deforum_parse(string, prompt_parser=None):
     return frames
 
 
+def to_keyframed(curve, n=None):
+    """
+    dummy-proof coersion
+    """
+    if isinstance(curve, Keyframed):
+        k = curve
+    elif isinstance(curve, str):
+        k = Keyframed.from_string(curve)
+    elif isinstance(curve, dict):
+        k = Keyframed(curve)
+    if n:
+        k.set_length(n)
+    return k
+
 
 class Keyframed:
     def __init__(
