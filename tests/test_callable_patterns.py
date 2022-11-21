84b4090ea09158ab0bfbc9233370f8d5df5fd665
David Marx <david.marx84@gmail.com>
2022-11-20 20:40:32 -0500

    polynomial interpolation cheat code

--- tests/test_callable_patterns.py
+++ tests/test_callable_patterns.py
@@ -4,6 +4,8 @@ Testing recipes for fancy applications of callable getters
 
 from keyframed import Keyframed
 
+TEST_EPS = 1e-8
+
 # implement fibonnaci
 def test_fib():
     fib_seq = Keyframed({0:1,1:1})
@@ -32,10 +34,20 @@ def test_fib_jump():
 
 # polynomial interpolation
 # https://docs.scipy.org/doc/scipy/tutorial/interpolate.html
+from scipy.interpolate import interp1d
 
 def test_quad():
-    seq={0:1,2:4}
-    pass
+    #seq={0:1,1:2, 3:8, 4:16} # this is exponential growth, not quadratic
+    seq={0:0,1:1,3:9,4:16}
+    K = Keyframed(seq)
+    xs = list(K.keyframes)
+    ys = [K[i] for i in xs]
+    def quad_interp(k):
+        f = interp1d(xs, ys, kind='quadratic')
+        return f(k).item()
+    K[2]=quad_interp
+    #assert abs(K[2] - 4) < TEST_EPS
+    assert 4-TEST_EPS <= K[2] <= 4+TEST_EPS
 
 # windowed average
 
