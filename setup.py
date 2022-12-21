3fa458510ce3b9243b51490da757c4c7c4038da5
David Marx <david.marx84@gmail.com>
2022-12-20 20:26:53 -0800

    apparently i don't know how semver works.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Keyframed is a time series data type that allows users to store and retrieve dat
         
 setup(
     name='keyframed',
-    version='0.0.3b',
+    version='0.0.4',
     author='David Marx',
     long_description=README,
     long_description_content_type='text/markdown',
