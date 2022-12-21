5ae1883425c720431a98e9dc57656d49ecbeb5ed
David Marx <david.marx84@gmail.com>
2022-12-20 20:17:58 -0800

    added short description + content type

--- setup.py
+++ setup.py
@@ -5,13 +5,18 @@ README = None
 if Path('README.md').exists():
     with open('README.md','r') as f:
         README = f.read()
-        README = README.replace('```python','```')
 
+st = """
+Keyframed is a time series data type that allows users to store and retrieve data at specified time indices. The motivation for this library is to facilitate object-oriented parameterization of generative animations.
+""".strip()
+        
 setup(
     name='keyframed',
     version='0.0.3b',
     author='David Marx',
     long_description=README,
+    long_description_content_type='text/markdown',
+    short_description=st,
     install_requires=[
         'traces',
         'numexpr',
