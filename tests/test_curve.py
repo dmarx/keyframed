b60ac67fb6bdae05ab76d8c536d756e2a5f04dad
David Marx <david.marx84@gmail.com>
2023-01-10 13:32:09 -0800

    removed old test that wasn't even doing anything

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -9,20 +9,6 @@ def test_curve():
 def test_prompt():
     p = Prompt("foo bar")
 
-def test_param_group():
-    c = Curve(((0,0), (1,1)))
-    p = Prompt("foo bar", weight=1.5)
-    settings = ParameterGroup({
-        'curve':c,
-        'prompt':p,
-        'scalar':10,
-    })
-    print(settings[0])
-    print(settings[1])
-    print(settings[2])
-    settings.visibility = Curve(((2,.5),))
-    print(settings[2])
-
 def test_curve_looping():
     curve = Curve(((0, 0), (9, 9)), loop=True)
     for i in range(20):
