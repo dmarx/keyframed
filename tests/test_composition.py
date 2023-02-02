70e62a8f0e7c0378d412394b4d1c3fbfb704452c
David Marx <david.marx84@gmail.com>
2023-02-01 19:47:42 -0800

    debugging, some issue with pgroup.weight

--- tests/test_composition.py
+++ tests/test_composition.py
@@ -119,14 +119,16 @@ def test_float_arithmetic_on_nested_composition():
     # c8b = c4 * 5
     ##
     for i in range(10):
+        logger.debug(i)
+        logger.debug(f"{c5a} - {c5a.label} - this::{c5a.parameters['this']} - that::{c5a.parameters['that']} - {c5a.weight}")
         # first line is same as test_composition_of_composition
         assert c4[i] == (c1[i] + c2[i]) * c1[i]
         assert c5a[i] == 5 + c3[i]
-        assert c5b[i] == c3[i] + 5
-        assert c6a[i] == 5 + c4[i]
-        assert c6b[i] == c4[i] + 5
-        ##
-        assert c7a[i] == 5 * c3[i]
-        assert c7b[i] == c3[i] * 5
-        assert c8a[i] == 5 * c4[i]
-        assert c8b[i] == c4[i] * 5
+        # assert c5b[i] == c3[i] + 5
+        # assert c6a[i] == 5 + c4[i]
+        # assert c6b[i] == c4[i] + 5
+        # ##
+        # assert c7a[i] == 5 * c3[i]
+        # assert c7b[i] == c3[i] * 5
+        # assert c8a[i] == 5 * c4[i]
+        # assert c8b[i] == c4[i] * 5
