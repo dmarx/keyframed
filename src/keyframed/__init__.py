e54870235f82a1d94c40340d710db05bbd462df4
David Marx <david.marx84@gmail.com>
2023-02-04 14:40:53 -0800

    simplified main api

--- src/keyframed/__init__.py
+++ src/keyframed/__init__.py
@@ -1,32 +1,21 @@
 from .curve import (
     Composition,
     Curve,
-    EaseIn, 
-    EaseOut,
     Keyframe,
     ParameterGroup,
     SmoothCurve,
 )
 from .interpolation import (
     bisect_left_keyframe, 
-    bisect_left_value, 
     bisect_right_keyframe, 
-    bisect_right_value, 
     register_interpolation_method,
-    eased_lerp,
-    #INTERPOLATORS,
 )
 
 __all__ = [
     'bisect_left_keyframe',
-    'bisect_left_value',
     'bisect_right_keyframe',
-    'bisect_right_value',
     'Composition',
     'Curve',
-    'eased_lerp',
-    'EaseIn', 
-    'EaseOut',
     'Keyframe',
     'ParameterGroup',
     'register_interpolation_method',
