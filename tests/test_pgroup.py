3fb386be49448a86be7cd28b1cc11471c0fec856
David Marx <david.marx84@gmail.com>
2023-02-04 16:33:32 -0800

    rm duplicate test

--- tests/test_pgroup.py
+++ tests/test_pgroup.py
@@ -19,17 +19,9 @@ def test_parameter_group_getitem():
     assert pgroup[0] == {'p1': 2, 'p2': 4}
 
 
-def test_parameter_group_getitem():
-    pgroup = ParameterGroup({'p1': 1, 'p2': 2}, weight=2)
-    assert pgroup[0] == {'p1': 2, 'p2': 4}
-
-
-
 def test_pgroup_nontrivial():
     pgroup = ParameterGroup({'p1': 1, 'p2': 2}, weight=2)
     assert pgroup.weight[0] == 2
     assert pgroup.weight[1] == 2
     assert pgroup[0] == {'p1': 2, 'p2': 4}
     assert pgroup[1] == {'p1': 2, 'p2': 4}
-
-###############################################
