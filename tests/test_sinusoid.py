4a288aa27b6cd4c7929c4bb9b1dcabb48a69b81e
David Marx <david.marx84@gmail.com>
2023-02-24 15:19:19 -0800

    moar tests

--- tests/test_sinusoid.py
+++ tests/test_sinusoid.py
@@ -12,3 +12,24 @@ def test_sine_simple():
     assert abs(c[2*math.pi] - 0) < EPS
     assert abs(c[math.pi/2] - 1) < EPS
     assert abs(c[3*math.pi/2] +1) < EPS
+
+    c = SinusoidalCurve(wavelength=2)
+    assert abs(c[0] - 0) < EPS
+    assert abs(c[1] - 0) < EPS
+    assert abs(c[2] - 0) < EPS
+    assert abs(c[1/2] - 1) < EPS
+    assert abs(c[3/2] +1) < EPS
+
+    c = SinusoidalCurve(frequency=1/2)
+    assert abs(c[0] - 0) < EPS
+    assert abs(c[1] - 0) < EPS
+    assert abs(c[2] - 0) < EPS
+    assert abs(c[1/2] - 1) < EPS
+    assert abs(c[3/2] +1) < EPS
+
+    c = SinusoidalCurve(frequency=1/2, amplitude=3)
+    assert abs(c[0] - 0) < EPS
+    assert abs(c[1] - 0) < EPS
+    assert abs(c[2] - 0) < EPS
+    assert abs(c[1/2] - 3) < EPS
+    assert abs(c[3/2] +3) < EPS
