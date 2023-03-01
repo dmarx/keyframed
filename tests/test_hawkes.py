e9dfacd645517cd51f318592cd00326aa53cbd39
David Marx <david.marx84@gmail.com>
2023-03-01 14:06:19 -0800

    some minimal testing for hawkes

---
+++ tests/test_hawkes.py
@@ -0,0 +1,16 @@
+from keyframed import HawkesProcessIntensity
+import pytest
+import math
+
+EPS = 1e-9
+
+def test_hawkes_simple():
+    c = HawkesProcessIntensity()
+    with pytest.raises(ValueError):
+        assert c[1] == 0 # empty hawkes process makes the thing angry
+    c.add_event(1)
+    assert c[1] == 1
+    assert c[0] == 0
+    assert c[0.5] == 0
+    assert abs(c[2] - 0.951229424500714) < EPS
+    
